49ad540a5d5cfaa6d3fc57943ded75c90178cbec
loriyuhv <2385923602@qq.com>
2021-11-05 18:50:17 +0800

    commit change @loriyuhv

--- FrontEnd/Html/excercise/jingdong/app01/views.py
+++ FrontEnd/Html/excercise/jingdong/app01/views.py
@@ -4,5 +4,20 @@ from django.shortcuts import render
 
 
 def jerry(req):
+    # print(req.GET)
+    # <QueryDict: {'username': ['loriyuhv'], 'password': ['123456'], 'sex': ['0'], 'hobby': ['basketball', 'football']}>
 
-    return render()
+    print(req.POST)
+    # 注意，要注释：MIDDLEWARE 里的'django.middleware.csrf.CsrfViewMiddleware'
+    # < QueryDict: {'username': ['loriyuhv'], 'password': ['12345'], 'sex': ['0'], 'hobby': ['basketball']} >
+
+    print("file：", req.FILES)
+    # 写入硬盘
+    for item in req.FILES:
+        obj = req.FILES.get(item)
+        file_name = obj.name
+        with open(file_name, "wb") as f:
+            for line in obj.chunks():
+                f.write(line)
+    return render(req, "index.html")
+
