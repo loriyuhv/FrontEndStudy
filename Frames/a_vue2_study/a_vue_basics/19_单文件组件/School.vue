e07b7eff7ee072a4c8aeb56c6103a99f65326d44
loriyuhv <2385923602@qq.com>
2024-04-05 18:18:44 +0800

    Vue之单文件组件学习

---
+++ Frames/a_vue2_study/a_vue_basics/19_单文件组件/School.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学校名称：{{ schoolName }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">学校名称</button>
+  </div>
+</template>
+
+<script>
+  // 组件交互相关的代码（数据、方法等等）
+  export default {
+    name: 'School',
+    data(){
+      return {
+        schoolName: '南昌工学院',
+        address: '江西省南昌市',
+      }
+    },
+    methods: {
+      showName() {
+        alert(this.schoolName)
+      }
+    }
+  }
+
+  // export default school
+</script>
+
+<style>
+   /* 组件的样式 */
+   .demo {
+    background-color: orange;
+   }
+</style>
---
+++ Frames/a_vue2_study/a_vue_basics/19_单文件组件/Student.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 组件的结构 -->
+  <div>
+    <h2>学生姓名：{{ studentName }}</h2>
+    <h2>学生地址：{{ age }}</h2>
+    <button @click="showName">学生姓名</button>
+  </div>
+</template>
+
+<script>
+  // 组件交互相关的代码（数据、方法等等）
+  export default {
+    name: 'Student',
+    data(){
+      return {
+        studentName: 'Jerry',
+        age: 18,
+      }
+    },
+    methods: {
+      showName() {
+        alert(this.studentName)
+      }
+    }
+  }
+
+  // export default school
+</script>
